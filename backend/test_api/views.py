f74f540aa0e295825845aef3341caef39c583083
ilija <ilijapet@gmail.com>
2024-05-24 23:37:14 +0200

    Adding fully functinoal Django backend with signin, sign out, rest password and other basic functinoalities

--- backend/test_api/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
-
-
-
-class TestAPIView(APIView):
-    def get(self, request):
-        return Response({"message": "Hello, world!"})
-    

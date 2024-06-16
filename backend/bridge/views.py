ce20b1dc13993774aca613174ab61b829aa90779
ilija <ilijapet@gmail.com>
2024-06-16 22:27:19 +0200

    resolving security vurnability of underscore 1.6.0 by upgrading to 1.13.6

--- backend/bridge/views.py
+++ backend/bridge/views.py
@@ -1,7 +1,4 @@
-from rest_framework.response import Response
 from rest_framework.views import APIView
-from utils.protocol import WitnessProtocol
-import base64
 from rest_framework.response import Response as HTTPResponse
 from utils.sc_interface import SCInterface
 

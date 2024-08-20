451cde83932027a51a4f6a22fa4e57f0b7c5cc0a
ilija <ilijapet@gmail.com>
2024-08-20 11:34:18 +0200

    update

--- backend/bridge/views.py
+++ backend/bridge/views.py
@@ -1,6 +1,5 @@
 from rest_framework.response import Response as HTTPResponse
 from rest_framework.views import APIView
-
 from utils.sc_interface import SCInterface
 
 
@@ -17,7 +16,7 @@ class BridgeView(APIView):
                 "signature": data["signature"],
                 "public_key": data["public_key"],
             }
-            self.contract.sendInput(value)
+            tx_hash = self.contract.sendInput(value)
             return HTTPResponse(status=200)
         except Exception as e:
             return HTTPResponse(status=500, data={"error": str(e)})
